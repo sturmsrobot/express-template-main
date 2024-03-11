848bcb944b8b256f2563a0f1295662a1da64ad2b
sturms_robot <151828922+sturmsrobot@users.noreply.github.com>
2024-03-11 14:26:56 +0100

    Aufgabe2

--- tests/routes/todos/TodosRoute.test.js
+++ tests/routes/todos/TodosRoute.test.js
@@ -11,3 +11,21 @@ describe("GET /v1/todos/all", () => {
     expect(response.body).toEqual([]);
   });
 });
+
+describe("GET /v1/todos/byid", () => {
+  test("responds with json", async () => {
+    // angenommen, ich habe ein todo mit der id"1234"
+    const todoId = "1234";
+    const response = await request(app)
+      .get("/v1/todos/byid?todoId=${todoId}")
+      .expect("Content-Type", /json/)
+      .expect(200);
+
+    // hier wird erwartet, dass antwort entsprechende info über todo-id enthält
+    expect(response.body).toEqual({
+      id: todoId,
+      task: "Irgendein Todo",
+      isDone: false,
+    });
+  });
+});
