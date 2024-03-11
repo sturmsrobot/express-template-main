7b1f9cffd13bd56405c0529e9340a74887b0e99b
sturms_robot <151828922+sturmsrobot@users.noreply.github.com>
2024-03-11 13:00:16 +0100

    test_config_unterricht

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const { AppRouter } = require("./src/routes");
+
+// Initialisierung von expres
+const app = express();
+app.use(bodyParser.json());
+
+// Use for development
+app.use(cors());
+app.use("/v1", AppRouter);
+
+module.exports = app;
